2ff5e4027053e8e1655ccf4980411048aa8d6cc6
Olivier Verdier <olivier.verdier@gmail.com>
2018-06-21 09:18:43 +0200

    travis continuous integration

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from distutils.core import setup
 
 setup(name='diffeopt',
       version='0.1',
-      packages=['diffeopt'],
+      packages=['diffeopt',
+                'diffeopt.action',
+                'diffeopt.cometric',
+                'diffeopt.distance',
+      ],
       )
