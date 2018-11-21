bb843343adb7e943e285a63f7cd550b0cc4625a7
Olivier Verdier <olivier.verdier@gmail.com>
2018-11-21 19:44:53 +0100

    better scaling for random diffeos
    that scaling seems to be independent from the resolution

--- diffeopt/utils.py
+++ diffeopt/utils.py
@@ -27,12 +27,14 @@ def normalize(I):
 
 
 from diffeopt.cometric import laplace
+
 def get_random_diffeo(group, nb_steps=10, scale=1.):
     cometric = laplace.get_laplace_cometric(group, s=2)
     rm = np.random.randn(*group.zero().shape)
     rv = cometric(rm)
     vmx = rv.abs().max()
-    rv_ = 2*rv/vmx/nb_steps*scale
+    shape_scale = (group.shape[0] + group.shape[1])/2
+    rv_ = rv/nb_steps/shape_scale**2*scale*32
     current = group.element()
     for i in range(nb_steps):
         current = group.exponential(rv_).compose(current)
--- tests/test_group.py
+++ tests/test_group.py
@@ -30,7 +30,7 @@ def test_integ():
 
 def test_inverse():
     group = DiffeoGroup((16,16))
-    defm = get_random_diffeo(group)
+    defm = get_random_diffeo(group, scale=.1)
     aid = defm.compose(defm.inverse())
     rid = group.get_raw_identity()
     assert np.allclose(rid.numpy(), aid.forward.numpy(), rtol=1e-9, atol=1e-1)
